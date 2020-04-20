0312c239d7ea6d7a7d9c5134e1fbdd4358ebb979
Sau <sau@Khais-MacBook-Pro.local>
2020-04-20 01:43:32 -0400

    return restaurant object

--- src/app/add-restaurant/add-restaurant.component.ts
+++ src/app/add-restaurant/add-restaurant.component.ts
@@ -9,7 +9,6 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 })
 export class AddRestaurantComponent implements OnInit {
   user_id: any
-  // restaurant: any
   restaurantForm: FormGroup
   food_types: any
   restaurant_types: any
@@ -18,31 +17,57 @@ export class AddRestaurantComponent implements OnInit {
     email: '',
     address: '',
     phone_number: '',
-    restaurant_types_id: null
-
+    restaurant_type: {
+      id: null,
+      type_name: ''
+    },
+    food_types: [{
+      id: null,
+      food_type: ''
+    }]
   } 
 
   constructor(private appService: FoodyService, private form: FormBuilder) { }
 
   ngOnInit() {
-    this.restaurantForm = this.form.group({
+    this.restaurantForm =  this.form.group({
       name: '',
       email: '',
       address: '',
-      phone_number: '',
-      foodType: [],
-      restaurantType: []
+      phoneNumber: '',
+      foodTypes: null,
+      restaurantType: null
     })
     this.allRestaurantTypes()
     this.allFoodTypes()
   }
 
-  addRestaurant(restaurant: any){
-    this.appService.createNewRestaurant(this.user_id, restaurant).subscribe(restaurant=>{
-    //   this.restaurant = restaurant
-    //   console.log("Restaurant after created: "+this.restaurant);
-      
+  addRestaurant(restaurant_value: any){
+    // this.food_types=restaurant.id.value();
+    console.log("restaurant.  important:  "+ JSON.stringify(restaurant_value));
+    this.restaurant=restaurant_value;
+    // this.restaurant.restaurant_types.id=restaurant_value.id
+    this.restaurant.restaurant_type=this.restaurant_types.filter(type=>{
+      return restaurant_value.restaurantType===type.id 
     })
+    console.log("Restaurant Type Object: "+JSON.stringify(this.restaurant.restaurant_type));
+    restaurant_value.foodTypes.forEach(foodType=>{
+      this.restaurant.food_types=this.food_types.filter(type=>{
+        // console.log("Food Type id: "+type.id+ "foodType : "+ foodType);
+        // if(type.id===foodType){
+        //   console.log('yes '+ type.food_type);
+        //   console.log('Food Type: '+ JSON.stringify(type));
+        // }
+        return type.id===foodType
+      })
+    })
+    console.log("Foodtype Object: " + JSON.stringify(this.restaurant.food_types));
+    
+    // this.appService.createNewRestaurant(this.user_id, restaurant).subscribe(restaurants=>{
+    //   this.restaurant = restaurant
+    //   console.log("Restaurant after created: "+restaurants);
+      
+    // })
   }
 
   allRestaurantTypes(){
@@ -68,5 +93,12 @@ export interface Restaurant{
   email: String,
   name: String,
   phone_number: String,
-  restaurant_types_id: Number
+  restaurant_type: {
+    id: Number,
+    type_name: String
+  },
+  food_types: [{
+    id: Number,
+    food_type: String
+  }]
 }
