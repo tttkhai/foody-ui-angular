b0b8517cfbaa6bc485c68230fab94d8c8a9d0ab9
Khai Tran <tttkhai93@gmail.com>
2020-07-16 02:22:33 -0400

    separate rating star of each service

--- src/app/rating/rating.component.ts
+++ src/app/rating/rating.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-rating',
@@ -6,12 +6,31 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./rating.component.css']
 })
 export class RatingComponent {
-  @Input() activeStars: number;
+  @Input() service: string;
+  @Output() cleanlinessStar= new EventEmitter<number>();
+  @Output() customerServiceStar= new EventEmitter<number>();
+  @Output() deliveryStar= new EventEmitter<number>();
+  @Output() tasteStar= new EventEmitter<number>();
+
   stars: number[] = [1, 2, 3, 4, 5];
   selectedValue: number;
-  
+
   countStar(star) {
     this.selectedValue = star;
     console.log('Value of star', star);
+    if(this.service==="cleanliness"){
+      this.cleanlinessStar=star
+    } 
+    if(this.service==="customer_service"){
+      this.customerServiceStar=star
+    } 
+    if(this.service==="deliver"){
+      this.cleanlinessStar=star
+    } 
+    if(this.service==="taste"){
+      this.deliveryStar=star
+    } 
   }
+
+
 }
